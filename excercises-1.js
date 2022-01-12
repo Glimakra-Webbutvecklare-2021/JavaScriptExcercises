48d63f83e28348e6ecf4e009e279a514a6854786
unknown <anders.sjunnesson@gmail.com>
2022-01-12 08:58:50 +0100

    Some functions

---
+++ excercises-1.js
@@ -0,0 +1,19 @@
+// Övningar i att skapa olika funktioner
+
+// 1. Skapa en funktion som använder metoden console.log() för att skriva ut strängen "Hello" följt av en textsträng
+
+// 2. Skapa en funktion som returnerar summan av två tal, om talen inte har datatypen number retunera undefined
+
+// 3. Skapa en funktion som anger en e-post adress baserat på förnamn, efternamn och ett domännamn. åäö ska ersättas av ao, VERSALER ska ersättas till gemener
+
+// 4. Skapa en funktion som beräknar kvadratroten av ett tal avrundat till 2 decimaler, om talet inte har datatypen number retunera undefined
+
+// 5. Skapa en funktion som slumpar fram ett värde (med Math.random) som antingen är "krona" eller "klave" - jämför att kasta ett mynt
+
+// 6. Skapa en funktion som tar bort blanksteg som inleder eller avslutar en textsträng. Funktionen ska returnera en korrigerad textsträng, och samtidigt tala om hur många tecken som tagits bort 
+
+// 7. Skapa en funktion som returnerar avslutande tecken efter sista punkten i ett domännamn ( .se .com ) etc
+
+// 8. Skapa en funktion som berättar vilket datum det är YYYY-mm-dd
+
+// 9. Skapa en funktion som skriver ut "Förmiddag" eller "Eftermiddag" beronde på aktuell tid
